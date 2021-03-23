98a895670187ee10f7c7426579e9abb26fe57fcf
Dariusz Jędrzejczak <dariusz@standardnotes.org>
2021-03-23 19:52:24 +0100

    fix: remove last todo comment

--- src/Domain/Operation/OperationValidator.ts
+++ src/Domain/Operation/OperationValidator.ts
@@ -4,7 +4,6 @@ import { Resource } from '../Resource/Resource'
 import { UserPermission } from '../User/UserPermission'
 import { Operation } from './Operation'
 
-// todo: https://app.asana.com/0/1199914526147392/1200093443903077/f
 @injectable()
 export class OperationValidator {
   isOperationPermitted = ({
