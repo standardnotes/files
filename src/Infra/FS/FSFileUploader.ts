3ec83df22e52afc1ae19693211ed84606e3adb45
Karol Sójko <karolsojko@protonmail.com>
2022-02-03 14:52:54 +0100

    fix: create directory before write stream

--- src/Infra/FS/FSFileUploader.ts
+++ src/Infra/FS/FSFileUploader.ts
@@ -1,4 +1,5 @@
-import { createWriteStream } from 'fs'
+import { createWriteStream, mkdirSync, mkdtempSync } from 'fs'
+import { dirname } from 'path'
 import { injectable } from 'inversify'
 import { Writable } from 'stream'
 
@@ -7,6 +8,8 @@ import { FileUploaderInterface } from '../../Domain/Services/FileUploaderInterfa
 @injectable()
 export class FSFileUploader implements FileUploaderInterface {
   createUploadStream(filePath: string): Writable {
+    mkdirSync(dirname(`${__dirname}/tmp/${filePath}`), { recursive: true })
+
     return createWriteStream(`${__dirname}/tmp/${filePath}`, { flags: 'w+' })
   }
 }
